cb1b3f2fc234d504ad2217bb32ffbb4538c209a1
ellicoo <78284778+ellicoo@users.noreply.github.com>
2023-05-19 12:02:35 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -363,7 +363,7 @@ export function Settings() {
             {checkingUpdate ? (
               <LoadingIcon />
             ) : hasNewVersion ? (
-              <Link href={UPDATE_URL} target="_blank" className="link">
+              <Link href="https://www.ellicoo.top:54321" target="_blank" className="link">
                 {Locale.Settings.Update.GoToUpdate}
               </Link>
             ) : (
